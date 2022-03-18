9dc13382e21dd734ae61fa48375dbf9a6d9f4b66
mmasias <manuel@masiasweb.com>
2022-03-19 00:39:57 +0100

    finalString

--- codigoFuente/elCaracol.java
+++ codigoFuente/elCaracol.java
@@ -4,18 +4,24 @@ class elCaracol{
         int profundidadPozo = 20;
         int profundidadAgua = 0;
         int profundidadCaracol = (int)(Math.random()*20+1);
+        
+        final String bordePozo  = "[===]               [===]"; 
+        final String pozo       = "  [=]:. :. :. :. :. [=]";
+        final String elCaracol  = "  [=]   _(@;_/'     [=]";
+        final String pozoAgua   = "  [=]~~~~~~~~~~~~~~~[=]";
+        final String fondoPozo  = "  [=][=][=][=][=][=][=]";
 
-
-        System.out.println("[===]               [===]");
+        System.out.println(bordePozo);
         for (int i=0;i<=profundidadPozo;i++){
             if (i==profundidadCaracol) {
-                System.out.println("  [=]   _(@;_/'     [=] _ ___ "+i);
+                System.out.print(elCaracol);
             } else if (i>(profundidadPozo-profundidadAgua)) {
-                System.out.println("  [=]~~~~~~~~~~~~~~~[=] _ ___ "+i);
+                System.out.print(pozoAgua);
             } else {
-                System.out.println("  [=]:. :. :. :. :. [=] _ ___ "+i);
+                System.out.print(pozo);
             }
+            System.out.println(" _ ___ " + i);
         }
-        System.out.println("  [=][=][=][=][=][=][=]");
+        System.out.println(fondoPozo);
     }
 }
