f5eb9bdd865b22d2d3c9de781a635bd38a9146c4
veronika <vesparragozam@gmail.com>
2022-03-21 23:59:39 +0100

    Avances del Caracol

--- caracol.java
+++ caracol.java
@@ -8,30 +8,40 @@ public class caracol{
         double profundidadCaracol;
         double caracolSube;
         double caracolBaja;
+        double carro;
 
         dia = 50;
+
         for(int j=0; j<=dia; j++){
             clima = Math.random();
+            carro = Math.random();
             profundidad = 20;
             profundidadAgua = 1;
             profundidadCaracol = Math.random()*(20-10)+10;
             caracolSube = Math.random()*(4-1)+1;
             caracolBaja = Math.random()*(2-0)+0;
-                if(j<=dia){
-                    if(clima<=0.4){
+            if(profundidadCaracol>0){
+                if(j<dia){
+                    if(clima<=0.05){
                         profundidadAgua = profundidadAgua + 5;
                         System.out.println("llueve!");
-                    } else if (clima<=0.8){
+                    } else if (clima<=0.15){
                         profundidadAgua = profundidadAgua + 2;
                         System.out.println("llueve!");
+                    }
+                    if (carro>=0.35){
+                        caracolBaja = caracolBaja - 2;
+                        System.out.println("Carro!");
                     }
                     System.out.print("Dia ["+j+"] / ");
                     System.out.print("Sube: [" + ((double)Math.round(profundidadCaracol + caracolSube * 100d) / 100d) + "] / ");
                     System.out.println("Baja: [" + ((double)Math.round(profundidadCaracol + caracolBaja * 100d) / 100d) + "] / ");
                     System.out.println("Altura al final del día: [" + ((double)Math.round((profundidadCaracol + caracolSube) - caracolBaja * 100d) /100d) + "]");
-                } else {
-                    System.out.println("El caracol se murió de hambre");
-                }
+                    if(carro>=0.35){
+                        System.out.println("[__]    O=O     [__]");
+                    } else{
+                        System.out.println("[__]            [__]");
+                    }
                     for (int i=0; i<=profundidad; i++){
                         if(((int)profundidadCaracol)==i){
                             System.out.println("[]    _@)_/’    [] _ __ "+i);
@@ -42,6 +52,13 @@ public class caracol{
                         }
                     }
                 System.out.println("[][][][][][][][][]");
+                } else {
+                        System.out.println("Murio el dia ["+ j +"] a una profundidad de ["+ ((double)Math.round(profundidadCaracol * 100d) / 100d) +"]");
+                }
+            }
+            else {
+                System.out.println("El caracol salió en el dia ["+ j +"]");
+            }
         }
     }
 }
