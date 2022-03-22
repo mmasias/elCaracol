efe3d3bfc2a1be312cbcff8b7b895617b150b1a6
Jorge85015 <jorge.quinonez@alumnos.uneatlantico.es>
2022-03-22 11:49:04 +0100

    Actualizacion nombres de variables

--- RetoCaracol.java
+++ RetoCaracol.java
@@ -3,62 +3,73 @@ import java.text.DecimalFormat;
 public class RetoCaracol{
     public static void main(String[] args){
         
-        int profundidad, profundidadA, dia, contador;
-        double profundidadC,distanciaS,distanciaB,probabilidadC,probabilidadL;
+        int profundidadPozo, profundidadAgua, dia, contador;
+        double profundidadCaracol,distanciaSubida,distanciaBajada,probabilidadCoche,probabilidadLluvia;
 
-        profundidad = 20;
+        profundidadPozo = 20;
         dia = 0;
-        profundidadC = (Math.random()*(20-10))+10;
-        profundidadA = 0;
+        profundidadCaracol = (Math.random()*(20-10))+10;
+        profundidadAgua = 0;
         
-        System.out.println("La profundidad inicial del caracol es de: " + (int)profundidadC);
+        System.out.println("La profundidad inicial del caracol es de: " + (int)profundidadCaracol);
         System.out.println("");
 
-        while(dia<50 || profundidadC<=0){
+        while(dia<50 || profundidadCaracol<=0){
             
             contador =  1 + dia++;
 
+            probabilidadLluvia = Math.random();
+
+            if(probabilidadLluvia<=0.05){
+                profundidadAgua = profundidadAgua + 5;
+            }
+            else if(probabilidadLluvia<=0.10){
+                profundidadAgua = profundidadAgua + 2;
+            }
+
+            if(profundidadCaracol<profundidadAgua){
+                probabilidadCoche = profundidadAgua + 1;
+            }
+
+
             if(contador>9){
-                distanciaS = Math.random()*((3-1)+1);
-                distanciaB = Math.random()*((2-0)+0);
+                distanciaSubida = Math.random()*((3-1)+1);
+                distanciaBajada = Math.random()*((2-0)+0);
             }
             else if(contador>19){
-                distanciaS = Math.random()*((2-1)+1);
-                distanciaB = Math.random()*((2-0)+0);
+                distanciaSubida = Math.random()*((2-1)+1);
+                distanciaBajada = Math.random()*((2-0)+0);
             }
             else{
-                distanciaS = Math.random()*((4-1)+1);
-                distanciaB = Math.random()*((2-0)+0);
+                distanciaSubida = Math.random()*((4-1)+1);
+                distanciaBajada = Math.random()*((2-0)+0);
             }
+            probabilidadCoche = Math.random();
 
-            probabilidadC = Math.random();
-
-            if(probabilidadC<0.35){
-                profundidadC = (int)profundidadC - distanciaS + distanciaB + 2;
+            if(probabilidadCoche<0.35){
+                profundidadCaracol = (int)profundidadCaracol - distanciaSubida + distanciaBajada + 2;
             }
             else{
-                profundidadC = (int)profundidadC - distanciaS + distanciaB;
-            }
-            
-            probabilidadL = Math.random();
-
-            if(probabilidadL<=0.05){
-                profundidadA = profundidadA + 5;
-            }
-            else if(probabilidadL<=0.10){
-                profundidadA = profundidadA + 2;
+                profundidadCaracol = (int)profundidadCaracol - distanciaSubida + distanciaBajada;
             }
 
-            if((int)profundidadC>profundidad){
-                profundidadC = 20;
+            if((int)profundidadCaracol<profundidadAgua){
+                profundidadCaracol = profundidadAgua + 1;
             }
 
-            for(int nivel=0;nivel<=profundidad;nivel++){
+            if((int)profundidadCaracol>profundidadPozo){
+                profundidadCaracol = profundidadPozo - 1;
+            }
+
+            if(probabilidadCoche<0.35){
+                System.out.println("       0-=-0        ");
+            }
+            for(int nivel=0;nivel<=profundidadPozo;nivel++){
         
-                if(((int)profundidadC)==nivel){
+                if(((int)profundidadCaracol)==nivel){
                     System.out.println("[] :.  _@)_/'  :. [] _ __" + nivel);
                 }
-                else if(nivel>profundidad-profundidadA){
+                else if(nivel>profundidadPozo-profundidadAgua){
                     System.out.println("[] ~~~~~~~~~~~~~~ [] _ __" + nivel);
                 }
                 else{
@@ -71,22 +82,22 @@ public class RetoCaracol{
             DecimalFormat df = new DecimalFormat("0.00");
 
             System.out.print("Día: " + contador + "      ");
-            System.out.print("El caracol subió " + df.format(distanciaS) + "m" + " y bajó " + df.format(distanciaB) +"m" + "     ");
-            System.out.println("Este día el caracol está a: " + ((int)-profundidadC) + " metros" );
+            System.out.print("El caracol subió " + df.format(distanciaSubida) + "m" + " y bajó " + df.format(distanciaBajada) +"m" + "     ");
+            System.out.println("Este día el caracol está a: " + ((int)-profundidadCaracol) + " metros" );
             System.out.println("");
             
-            if(probabilidadL<=0.05){
+            if(probabilidadLluvia<=0.05){
                 System.out.println("Este día hubo lluvia fuerte.");
                 System.out.println("");
             }
-            else if(probabilidadL<=0.10){
+            else if(probabilidadLluvia<=0.10){
                 System.out.println("Este día hubo lluvia media.");
                 System.out.println("");
             }
-            if(probabilidadC<0.35){
+            if(probabilidadCoche<0.35){
                 System.out.println("Este día pasó un coche cerca del pozo y el caracol bajó 2 metros.");
             }
-            if((int)profundidadC<=0){
+            if((int)profundidadCaracol<=0){
                 System.out.println("El caracol logró salir del pozo en el día: " + contador);
                 System.out.println("-------------------------------------------------------------------------------------------------");
                 System.exit(1);
